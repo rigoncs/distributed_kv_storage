eda984a10d0e0d31220d34496816bb4b2f476357
rigoncs <2412789408@qq.com>
2025-05-23 00:24:04 +0800

    finish code

--- src/shardkv/conf_change.go
+++ src/shardkv/conf_change.go
@@ -40,6 +40,12 @@ func (kv *ShardKV) handleConfigChangeMessage(command RaftCommand) *OpReply {
 	case ConfigChange:
 		newConfig := command.Data.(shardctrler.Config)
 		return kv.applyNewConfig(newConfig)
+	case ShardMigration:
+		shardData := command.Data.(ShardOperationReply)
+		return kv.applyShardMigration(&shardData)
+	case ShardGC:
+		shardInfo := command.Data.(ShardOperationArgs)
+		return kv.applyShardGC(&shardInfo)
 	default:
 		panic("unknown config change type")
 	}
@@ -50,9 +56,17 @@ func (kv *ShardKV) applyNewConfig(newConfig shardctrler.Config) *OpReply {
 		for i := 0; i < shardctrler.NShards; i++ {
 			if kv.currentConfig.Shards[i] != kv.gid && newConfig.Shards[i] == kv.gid {
 				// shard 需要迁移进来
+				gid := kv.currentConfig.Shards[i]
+				if gid != 0 {
+					kv.shards[i].Status = MoveIn
+				}
 			}
 			if kv.currentConfig.Shards[i] == kv.gid && newConfig.Shards[i] != kv.gid {
 				// shard 需要迁移出去
+				gid := newConfig.Shards[i]
+				if gid != 0 {
+					kv.shards[i].Status = MoveOut
+				}
 			}
 		}
 		kv.prevConfig = kv.currentConfig
@@ -60,4 +74,47 @@ func (kv *ShardKV) applyNewConfig(newConfig shardctrler.Config) *OpReply {
 		return &OpReply{Err: OK}
 	}
 	return &OpReply{Err: ErrWrongConfig}
+}
+
+func (kv *ShardKV) applyShardMigration(shardDataReply *ShardOperationReply) *OpReply {
+	if shardDataReply.ConfigNum == kv.currentConfig.Num {
+		for shardId, shardData := range shardDataReply.ShardData {
+			shard := kv.shards[shardId]
+			// 将数据存储到当前 Group 对应的 shard 中
+			if shard.Status == MoveIn {
+				for k, v := range shardData {
+					shard.KV[k] = v
+				}
+				// 状态置为 GC，等待清理
+				shard.Status = GC
+			} else {
+				break
+			}
+		}
+
+		// 拷贝去重表数据
+		for clientId, dupTable := range shardDataReply.DuplicateTable {
+			table, ok := kv.duplicateTable[clientId]
+			if !ok || table.SeqId < dupTable.SeqId {
+				kv.duplicateTable[clientId] = dupTable
+			}
+		}
+	}
+	return &OpReply{Err: OK}
+}
+
+func (kv *ShardKV) applyShardGC(shardsInfo *ShardOperationArgs) *OpReply {
+	if shardsInfo.ConfigNum == kv.currentConfig.Num {
+		for _, shardId := range shardsInfo.ShardIds {
+			shard := kv.shards[shardId]
+			if shard.Status == GC {
+				shard.Status = Normal
+			} else if shard.Status == MoveOut {
+				kv.shards[shardId] = NewMemoryKVStateMachine()
+			} else {
+				break
+			}
+		}
+	}
+	return &OpReply{Err: OK}
 }
--- src/shardkv/server.go
+++ src/shardkv/server.go
@@ -77,7 +77,8 @@ func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
 
 func (kv *ShardKV) matchGroup(key string) bool {
 	shard := key2shard(key)
-	return kv.currentConfig.Shards[shard] == kv.gid
+	shardStatus := kv.shards[shard].Status
+	return kv.currentConfig.Shards[shard] == kv.gid && (shardStatus == Normal || shardStatus == GC)
 }
 
 func (kv *ShardKV) requestDuplicated(clientId, seqId int64) bool {
@@ -191,7 +192,13 @@ func (kv *ShardKV) killed() bool {
 // for any long-running work.
 func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister, maxraftstate int, gid int,
 	ctrlers []*labrpc.ClientEnd, make_end func(string) *labrpc.ClientEnd) *ShardKV {
+	// call labgob.Register on structures you want
+	// Go's RPC library to marshall/unmarshall
 	labgob.Register(Op{})
+	labgob.Register(RaftCommand{})
+	labgob.Register(shardctrler.Config{})
+	labgob.Register(ShardOperationArgs{})
+	labgob.Register(ShardOperationReply{})
 
 	kv := new(ShardKV)
 	kv.me = me
@@ -220,6 +227,9 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister,
 	kv.restoreFromSnapshot(persister.ReadSnapshot())
 
 	go kv.applyTask()
+	go kv.fetchConfigTask()
+	go kv.shardMigrationTask()
+	go kv.shardGCTask()
 	return kv
 }
 func (kv *ShardKV) applyToStateMachine(op Op, shardId int) *OpReply {
@@ -252,11 +262,18 @@ func (kv *ShardKV) makeSnapshot(index int) {
 	enc := labgob.NewEncoder(buf)
 	_ = enc.Encode(kv.shards)
 	_ = enc.Encode(kv.duplicateTable)
+	_ = enc.Encode(kv.currentConfig)
+	_ = enc.Encode(kv.prevConfig)
 	kv.rf.Snapshot(index, buf.Bytes())
 }
 
 func (kv *ShardKV) restoreFromSnapshot(snapshot []byte) {
 	if len(snapshot) == 0 {
+		for i := 0; i < shardctrler.NShards; i++ {
+			if _, ok := kv.shards[i]; !ok {
+				kv.shards[i] = NewMemoryKVStateMachine()
+			}
+		}
 		return
 	}
 
@@ -264,10 +281,17 @@ func (kv *ShardKV) restoreFromSnapshot(snapshot []byte) {
 	dec := labgob.NewDecoder(buf)
 	var stateMachine map[int]*MemoryKVStateMachine
 	var dupTable map[int64]LastOperationInfo
-	if dec.Decode(&stateMachine) != nil || dec.Decode(&dupTable) != nil {
+	var currentConfig shardctrler.Config
+	var prevConfig shardctrler.Config
+	if dec.Decode(&stateMachine) != nil ||
+		dec.Decode(&dupTable) != nil ||
+		dec.Decode(&currentConfig) != nil ||
+		dec.Decode(&prevConfig) != nil {
 		panic("failed to restore from snapshot")
 	}
 
 	kv.shards = stateMachine
 	kv.duplicateTable = dupTable
+	kv.currentConfig = currentConfig
+	kv.prevConfig = prevConfig
 }
